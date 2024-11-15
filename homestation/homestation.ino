0a3010308d60f616fd5285a13e46e7a0a4b0cd9a
Eliam Traas <eliamtraas@gmail.com>
2024-11-15 22:49:28 +0100

    comments

--- homestation/homestation.ino
+++ homestation/homestation.ino
@@ -1,24 +1,31 @@
 #include "DHT.h"
 #include <Arduino.h>
 
-float calculateAverage(float array[], int count) {
-  if (count == 0) return 0;
+// Functie om gemiddelde te berekenen
+float calculateAverage(float array[], int count)
+{
+  if (count == 0)
+    return 0;
   float total = 0;
-  for (int i = 0; i < count; i++) {
+  for (int i = 0; i < count; i++)
+  {
     total += array[i];
   }
   return total / count;
 }
 
+// Pin-definities voor display
 int RES_pin = 5;
 int RCK_pin = 0;
 int SCK_pin = 4;
 int RES_pin2 = 13;
 int RCK_pin2 = 14;
 int SCK_pin2 = 12;
-int dec_digits[12] = {3, 159, 37, 13, 153, 73, 65, 31, 1, 9, 254, 0};
+int dec_digits[13] = {3, 159, 37, 13, 153, 73, 65, 31, 1, 9, 254, 0, 255};
 
-void showData(int data) {
+// Functie om data op het display te tonen
+void showData(int data)
+{
   int tens = (data / 10) % 10;
   int ones = data % 10;
 
@@ -31,8 +38,11 @@ void showData(int data) {
   digitalWrite(RCK_pin2, HIGH);
 }
 
-void showDataIndex(int index1, int index2) {
-  if (index1 < 0 || index1 >= 12 || index2 < 0 || index2 >= 12) {
+// Functie om zelfgekozen cijfers op het display te tonen
+void showDataIndex(int index1, int index2)
+{
+  if (index1 < 0 || index1 >= 13 || index2 < 0 || index2 >= 13)
+  {
     Serial.println("Ongeldige index in dec_digits.");
     return;
   }
@@ -46,125 +56,139 @@ void showDataIndex(int index1, int index2) {
   digitalWrite(RCK_pin2, HIGH);
 }
 
-void weatherCode(int wind, int temp) {
-
-}
-
+// Functie om licht in lux te berekenen
+// Laat als enige 2 rondes 2 cijfers zien
 const int LIGHT_SENSOR_PIN = A0;
 int lux;
 
-void checkBrightness() {
+void checkBrightness()
+{
   float measurements[10];
-  
-  for (int i = 0; i < 10; i++) {
+
+  for (int i = 0; i < 10; i++)
+  {
     measurements[i] = analogRead(LIGHT_SENSOR_PIN);
     delay(500);
   }
 
   int average = calculateAverage(measurements, 10);
-  lux = (average  + 434.5) / 2.5226;
+  lux = (average + 434.5) / 2.5226;
 
   Serial.print("Hoeveelheid licht in lux = ");
   Serial.println(lux);
 
-  // Controleer of de waarde in het bereik van 1000 of meer ligt om deze te splitsen
-  if (lux >= 100) {
-    // Splits waarde in tweeën: eerst de eerste twee cijfers, daarna de laatste twee
-    int firstPart = lux / 100;    // Bijvoorbeeld, 1035 -> 10
-    int secondPart = lux % 100;   // Bijvoorbeeld, 1035 -> 35
-
-    // Toon eerst de eerste twee cijfers voor 3 seconden
+  if (lux >= 100)
+  {
+    int firstPart = lux / 100;
+    int secondPart = lux % 100;
     showData(firstPart);
     delay(3000);
-
-    // Toon vervolgens de laatste twee cijfers voor 3 seconden
     showData(secondPart);
     delay(3000);
-  } else {
+  }
+  else
+  {
     showData(00);
     delay(3000);
-    // Als de waarde minder dan 100 is, toon het hele getal voor 5 seconden
     showData(lux);
     delay(3000);
   }
 }
 
-
-#define DHTPIN 10         
-#define DHTTYPE DHT11    
+// Functie om temperatuur te meten
+#define DHTPIN 10
+#define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
-float checkTemperature() {
+float checkTemperature()
+{
   float temperatureArray[10];
   int validMeasurements = 0;
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++)
+  {
     float temperature = dht.readTemperature();
 
-    if (!isnan(temperature)) {
+    if (!isnan(temperature))
+    {
       temperatureArray[validMeasurements] = temperature;
       validMeasurements++;
     }
     delay(500);
   }
 
-  float avgTemperature = validMeasurements > 0 ? calculateAverage(temperatureArray, validMeasurements) : -999; // -999 betekent fout
+  float avgTemperature = validMeasurements > 0 ? calculateAverage(temperatureArray, validMeasurements) : -999;
 
-  if (avgTemperature != -999) {
+  if (avgTemperature != -999)
+  {
     Serial.print("Temperatuur in °C: ");
     Serial.println(avgTemperature);
     showData(avgTemperature);
     delay(5000);
-  } else {
-    showDataIndex(11, 11);  // Toont error code
+  }
+  else
+  {
+    showDataIndex(11, 11);
   }
 
   return avgTemperature;
 }
 
-void checkHumidity() {
+// Functie om luchtvochtigheid te meten
+float checkHumidity()
+{
   float humidityArray[10];
   int validMeasurements = 0;
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i++)
+  {
     float humidity = dht.readHumidity();
 
-    if (!isnan(humidity)) {
+    if (!isnan(humidity))
+    {
       humidityArray[validMeasurements] = humidity;
       validMeasurements++;
     }
     delay(500);
   }
 
-  if (validMeasurements > 0) {
-    int avgHumidity = calculateAverage(humidityArray, validMeasurements);
+  float avgHumidity = validMeasurements > 0 ? calculateAverage(humidityArray, validMeasurements) : -999;
+
+  if (avgHumidity != -999)
+  {
     Serial.print("Luchtvochtigheid in percentage waterdamp: ");
     Serial.println(avgHumidity);
     showData(avgHumidity);
     delay(5000);
-  } else {
-    showDataIndex(11, 11);  
   }
+  else
+  {
+    showDataIndex(11, 11);
+  }
+
+  return avgHumidity;
 }
 
-int pulseCount;
+// Functie om windsnelheid te meten
+// Functie doet hier 10 seconden over
 const int irSensorPin = 9;
-unsigned long startTime = 0;
-int windsnelheid;
 
-float checkWindSpeed() {
+float checkWindSpeed()
+{
   int pulseCount = 0;
   unsigned long startTime = millis();
-  unsigned long interval = 10000; // 10 seconden interval
+  unsigned long interval = 10000;
 
-  while (millis() - startTime < interval) {
-    if (digitalRead(irSensorPin) == LOW) {
+  while (millis() - startTime < interval)
+  {
+    if (digitalRead(irSensorPin) == LOW)
+    {
       pulseCount++;
-      delay(50);  // kort delay om debounce van sensor te voorkomen
+      delay(50);
 
-      // Wacht totdat de sensor niet meer LOW is om dubbel tellen te voorkomen
-      while (digitalRead(irSensorPin) == LOW) {
+      while (digitalRead(irSensorPin) == LOW)
+      {
         delay(1);
       }
     }
@@ -181,32 +205,42 @@ float checkWindSpeed() {
   return windsnelheid;
 }
 
-#define RED_LED_PIN D4 
+// Functie om LED's te laten branden bij bepaalde weersomstandigheden
+#define RED_LED_PIN D4
 #define ORANGE_LED_PIN D8
 #define GREEN_LED_PIN 1
 
-void weatherLED(int wind, int temp) {
-  // Controleer gevaarlijke weersomstandigheden
-  if (wind > 20 || temp > 35 || temp < -5) {
-    // Rood LED brandt bij gevaarlijke omstandigheden
+void weatherLED(float wind, float temp)
+{
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(ORANGE_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW);
+
+  if (wind > 20 || temp > 35 || temp < -5)
+  {
     digitalWrite(RED_LED_PIN, HIGH);
+    delay(4000);
     Serial.println("Gevaarlijke weersomstandigheden: rood LED aan.");
+    digitalWrite(RED_LED_PIN, LOW);
   }
-  // Controleer matige weersomstandigheden
-  else if (wind > 10 || temp < 0|| temp > 30) {
-    // Oranje LED brandt bij matige omstandigheden
+  else if (wind > 10 || (temp > 30 && temp < 0))
+  {
     digitalWrite(ORANGE_LED_PIN, HIGH);
+    delay(4000);
     Serial.println("Matige weersomstandigheden: oranje LED aan.");
+    digitalWrite(ORANGE_LED_PIN, LOW);
   }
-  // Goede weersomstandigheden
-  else {
-    // Groen LED brandt bij ideale omstandigheden
+  else
+  {
     digitalWrite(GREEN_LED_PIN, HIGH);
+    delay(4000);
     Serial.println("Goede weersomstandigheden: groen LED aan.");
+    digitalWrite(GREEN_LED_PIN, LOW);
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(RES_pin, OUTPUT);
@@ -214,39 +248,37 @@ void setup() {
   pinMode(SCK_pin, OUTPUT);
   pinMode(RES_pin2, OUTPUT);
   pinMode(RCK_pin2, OUTPUT);
-  pinMode(SCK_pin2, OUTPUT); 
+  pinMode(SCK_pin2, OUTPUT);
 
   dht.begin();
 
   pinMode(irSensorPin, INPUT);
 
-  pinMode(LED, OUTPUT); // set the digital pin as output.
-  pinMode(LED2, OUTPUT); // set the digital pin as output.
-  pinMode(LED3, OUTPUT); // set the digital pin as output.
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(ORANGE_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
 
-  // Zet alle LEDs uit
   digitalWrite(RED_LED_PIN, LOW);
   digitalWrite(ORANGE_LED_PIN, LOW);
   digitalWrite(GREEN_LED_PIN, LOW);
-
 }
 
-void loop() {
-  // Meting 1: Helderheid
-  showDataIndex(10, 1);  // Toon '8.1' voor 2 seconden
-  checkBrightness();  // Toon gemeten helderheid gedurende 5 seconden
-  
-  // Meting 2: Temperatuur
-  showDataIndex(10, 2);  // Toon '8.2' voor 2 seconden
-  checkTemperature();     // Toon gemeten temperatuur gedurende 5 seconden
+void loop()
+{
+  showDataIndex(10, 1);
+  checkBrightness();
 
-  // Meting 3: Luchtvochtigheid
-  showDataIndex(10, 3);  // Toon '8.3' voor 2 seconden
-  checkHumidity();        // Toon gemeten luchtvochtigheid gedurende 5 seconden
+  showDataIndex(10, 2);
+  float avgTemperature = checkTemperature();
 
-  // Meting 4: Windsnelheid
-  showDataIndex(10, 4);  // Toon '8.4' voor 2 seconden
-  checkWindSpeed();       // Toon gemeten windsnelheid gedurende 5 seconden
+  showDataIndex(10, 3);
+  checkHumidity();
+
+  showDataIndex(10, 4);
+  float windsnelheid = checkWindSpeed();
 
   weatherLED(windsnelheid, avgTemperature);
+
+  showDataIndex(12, 12);
+  delay(3000);
 }
